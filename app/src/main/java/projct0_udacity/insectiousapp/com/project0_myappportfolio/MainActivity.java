ae14461dff433f4395104dec0a4337c3e68f17ef
chetancodev95 <chetanmann95@gmail.com>
2016-10-21 12:25:56 +0530

    first commit

---
+++ app/src/main/java/projct0_udacity/insectiousapp/com/project0_myappportfolio/MainActivity.java
@@ -0,0 +1,13 @@
+package projct0_udacity.insectiousapp.com.project0_myappportfolio;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
